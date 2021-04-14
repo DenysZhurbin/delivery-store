e5cad2e0bb140f015ebcf0c72f3f3635edf61cd1
SoseD <sosedslim@gmail.com>
2021-04-14 19:17:17 +0300

    Add input constraint; iteration method changed

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ var state = {
     {
       name: "product-1",
       title: "Стул 1 из данных",
-      quantity: 345,
+      quantity: 3,
       orderAmount: 0
     },
     {
@@ -24,44 +24,39 @@ var state = {
 
 //Отрисовка разметки
 function render(data) {
-  var outputContainer = document.getElementsByClassName("container")[0];
+  var outputContainer = document.getElementsByClassName('container')[0];
   var products = data.products;
 
   while (outputContainer.firstChild) {
     outputContainer.removeChild(outputContainer.lastChild);
   }
 
-  for (var i = 0; i < products.length; i++) {
-    var product = products[i];
-    var productElement = document.createElement("div");
-    // classList.add добавляет имя класса к уже имеющемуюся блоку (dom node)
+  products.map(function(product, i) { 
+    var productElement = document.createElement('div');
+
     productElement.classList.add('product');
-    // innerHtml = принимает строку, которую браузер понимает как html (будет как дочерний контент dom node)
     productElement.innerHTML = `<div class="product__title">${product.title}</div>
       <div class="product__actions" data-product-index="${i}" data-product-name="${product.name}">
           <input type="text" value="${product.quantity}" class="form-control form-control--available-quantity" readonly>
           <input type="text" value="${product.orderAmount}" id="user-input" class="form-control form-control--order-amount">
-          <input type="text" value="${product.orderAmount}" class="form-control form-control--order-amount">
           <button type="button" class="btn btn-primary btn-increase-order-amount">Add</button>
       </div>`;
-    // запись вида ${product.quantity} - это современный синтаксис template string es6
 
     outputContainer.appendChild(productElement);
-  }
+  });
 }
 
 //Действия пользователя
 function handleUserAction() {
-  var productsElements = document.getElementsByClassName("product");
+  var productsElements = document.querySelectorAll('.product');
 
-  for (var i = 0; i < productsElements.length; i++) {
-    var product = productsElements[i];
-    var button = product.querySelectorAll(".btn-increase-order-amount")[0];
-    var userInput = product.querySelectorAll('#user-input')[0]; //document.getElementById('user-input');
+  productsElements.forEach(function(product) {
+    var button = product.querySelectorAll('.btn-increase-order-amount')[0];
+    var userInput = product.querySelectorAll('#user-input')[0];
 
-    button.addEventListener("click", function (event) {
+    button.addEventListener('click', function (event) {
       var button = event.target;
-      var productIndex = button.parentNode.getAttribute("data-product-index");
+      var productIndex = button.parentNode.getAttribute('data-product-index');
       var quantityValue = state.products[productIndex].quantity;
       var orderAmountValue = state.products[productIndex].orderAmount += 1;
       var input = button.parentNode.querySelectorAll('.form-control--order-amount')[0];
@@ -69,44 +64,33 @@ function handleUserAction() {
       if (orderAmountValue === quantityValue) {
         button.disabled = true;
       }
+
       input.value = orderAmountValue;   
     });
     
     userInput.addEventListener('keyup', function(event) {
-      var button = event.target;
-      var productIndex = button.parentNode.getAttribute("data-product-index");
+      var input = event.target;
+      var productIndex = input.parentNode.getAttribute('data-product-index');
       var quantityValue = state.products[productIndex].quantity;
-
-      if (event.target.value >= quantityValue) {
-        event.target.value = quantityValue;
-        console.log(quantityValue.value.length);
-        console.log(quantityValue);
+      
+      if (input.value >= quantityValue) {
+        input.value = quantityValue;
       }
     });
-
-      if (orderAmountValue === quantityValue) {
-        button.disabled = true;
-      }
-      input.value = orderAmountValue;  
-      console.log(quantityValue);
-    });
-//todo выровнять значение инпута пользователя, с quantity при максимальном значении. (работать с данніми, а не с инпутом).
-//querySelectorAll - прочесть как работает.
-  }
+  });
 }
 
+// function getOrderValues(state) {
+//   return {
 
-function getOrderValues(state) {
-  return {
-
-  }
-}
+//   }
+// }
 
 //Глобальная функция (запускает рабочие функции)
 function initCatalog() {
   render(state);
   handleUserAction();
-  getOrderValues(state);
+  //getOrderValues(state);
 }
 
 // note
